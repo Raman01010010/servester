e8a5149457de871662765734d800801ec9120565
Raman01010010 <rmnprjrrr@gmail.com>
2023-11-25 00:53:06 +0530

    minor chnage

--- routes/auth.js
+++ routes/auth.js
@@ -2,5 +2,5 @@ const express=require('express')
 const router=express()
 const authController=require('../controllers/authController')
 //const loginLimiter=require('../middleware/loginLimiter')
-router.post('/',loginLimiter,authController.handleLogin)
+router.post('/',authController.handleLogin)
 module.exports=router
